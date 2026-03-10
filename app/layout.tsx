ff5ae4fe7ae580429b7d11d618906d92076441da
Nyady <nyansafoaffum@gmail.com>
2026-03-10 15:11:51 +0000

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Nyansafo",
   description: "Nyansafo InsureFinTech ",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
